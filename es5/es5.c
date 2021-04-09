e30cf3e2bee00a3d38e696ed5b4cc1c23f5ed86c
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-04-10 00:15:17 +0200

    es5

---
+++ es5/es5.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int *join_arrays_same_length(int *array1, int *array2, int size);
+
+int main() {
+    int a1[] = {0, 1, 2, 3, 4};
+    int a2[] = {4, 3, 2, 1, 0};
+
+    int *a3 = join_arrays_same_length(a1, a2, 5);
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", a3[i]);
+    }
+
+    exit(0);
+}
+
+int *join_arrays_same_length(int *array1, int *array2, int size) {
+    int *fusion = calloc(size, sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
+        fusion[i] = array1[i] > array2[i] ? array1[i] : array2[i];
+    }
+    return fusion;
+}
