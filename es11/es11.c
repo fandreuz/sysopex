06a0979c7a838146a0b3793aca16e81183a9fb98
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-04-10 15:55:41 +0200

    prima parte es11

--- es11/es11.c
+++ es11/es11.c
@@ -33,9 +33,13 @@ int main(int argc, char** argv) {
 
     char buffer[100];
     double loadavg[3];
+    float averages_sum = 0;
+
     for (int i = 0; i < numero_cicli; i++)
     {
         getloadavg(loadavg, 3);
+
+        averages_sum += loadavg[0];
 
         int printed_number = sprintf(buffer, "%f\n", loadavg[0]);
         if (printed_number <= 0) {
@@ -51,6 +55,67 @@ int main(int argc, char** argv) {
 
         printf("%d\n", i);
         sleep(intervallo);
+    }
+
+    int pid = fork();
+    if (pid == 0) {
+        // figlio
+        float result = averages_sum / numero_cicli;
+        printf("Average: %f\n", result);
+
+        int result_fd = open("result.txt", O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+        if (result_fd == -1) {
+            perror("open");
+            exit(1);
+        }
+
+        char result_buffer[100];
+        int sres = sprintf(result_buffer, "%f\n", result);
+        if (sres < 1) {
+            perror("sprintf");
+            exit(1);
+        }
+
+        int wres = write(result_fd, result_buffer, 100);
+        if (wres <= 0) {
+            perror("write");
+            exit(1);
+        }
+    }
+    else if (pid > 0) {
+        // padre
+        int wres = wait(&pid);
+        if (wres == -1) {
+            perror("wait");
+            exit(1);
+        }
+
+        int result_fd = open("result.txt", O_RDONLY);
+        if (result_fd == -1) {
+            perror("open");
+            exit(1);
+        }
+
+        char buffer[100];
+        int rres = read(result_fd, buffer, 100);
+        if (rres <= 0) {
+            perror("read");
+            exit(1);
+        }
+
+        float written_average;
+        int sres = sscanf(buffer, "%f", &written_average);
+        if (sres <= 0) {
+            perror("sscanf");
+            exit(1);
+        }
+
+        printf("child ha scritto %f\n", written_average);
+    }
+    else
+    {
+        perror("fork");
+        exit(1);
     }
 
     return 0;
